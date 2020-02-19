fa669955309704272164f642adccd5099f3ca6db
Мельников Сергей Аркадьевич <SMelnikov2@alfabank.ru>
2020-02-19 22:22:45 +0300

    Create project

---
+++ Learning/Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning
+//
+//  Created by Сергей Мельников on 19.02.2020.
+//  Copyright © 2020 Сергей Мельников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
