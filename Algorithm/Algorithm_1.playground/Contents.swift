7e4eed1c6e46b1e9cc980411997fe2da8a0271c0
ЧСВ драйверок <fmlpwnz@MacBookBuild.local>
2020-02-20 22:32:16 +0300

    bla

--- Algorithm/Algorithm_1.playground/Contents.swift
+++ Algorithm/Algorithm_1.playground/Contents.swift
@@ -1,18 +1,13 @@
 import UIKit
 
 func sort(_ array: [Int]) -> [Int] {
-    return []
+    var result = array
+    for item in result.enumerated() {
+        if result[item.offset] < 2 {
+            print("Njkzy gblh")
+        }
+        print(item)
+    }
+    return result
 }
-var testNumber = 0
-testNumber += 1
-sort([1,2,4,3]) == [1,2,3,4] ? print("Супер, тест №\(testNumber) пройден") : print("Отстой, тест №\(testNumber) не пройден")
-testNumber += 1
-sort([]) == [] ? print("Супер, тест №\(testNumber) пройден") : print("Отстой, тест №\(testNumber) не пройден")
-testNumber += 1
-sort([5,3,1]) == [1,3,5] ? print("Супер, тест №\(testNumber) пройден") : print("Отстой, тест №\(testNumber) не пройден")
-testNumber += 1
-sort([3,1,4,2]) == [1,2,3,4] ? print("Супер, тест №\(testNumber) пройден") : print("Отстой, тест №\(testNumber) не пройден")
-testNumber += 1
-sort([1,2,2,3]) == [1,2,2,3] ? print("Супер, тест №\(testNumber) пройден") : print("Отстой, тест №\(testNumber) не пройден")
-testNumber += 1
-sort([1,-1,0]) == [-1,0,1] ? print("Супер, тест №\(testNumber) пройден") : print("Отстой, тест №\(testNumber) не пройден")
+sort([1,3,2,4])
